52d13eb71b2772e575bbe6208eec707fb8e8f15e
Guillermo Zaandam <guillermozaandam@hotmail.com>
2022-11-14 10:39:30 +0000

    datetime

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', datetime = str(datetime.now()))
 
 @app.route('/user/<name>')
 def user(name):
