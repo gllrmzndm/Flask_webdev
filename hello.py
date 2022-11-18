b4563f990129aafa9a0ffbd6eb9b8a8f84cca07c
Guillermo Zaandam <guillermozaandam@hotmail.com>
2022-11-18 10:33:25 +0000

    form works with good redirects

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -9,19 +9,20 @@ from datetime import datetime
 from flask_wtf.csrf import CSRFProtect
 
 # CSRF protection
-
 csrf = CSRFProtect()
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField("Submit")
+
 
 app = Flask(__name__)
 
+# Import os library to generate random SECRENT_KEY
 import os
 
 SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config["SECRET_KEY"] = SECRET_KEY
 
 csrf.init_app(app)
 
@@ -29,27 +30,27 @@ bootstrap = Bootstrap(app)
 
 
 # Routes
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session["name"] = form.name.data
+        return redirect(url_for("index"))
+    return render_template("index.html", form=form, name=session.get("name"))
 
 # return render_template('index.html', datetime = str(datetime.now()))
 
-@app.route('/user/<name>')
+
+@app.route("/user/<name>")
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template("user.html", name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template("500.html"), 500
